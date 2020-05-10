45cf6589f816d1ef19b355385535f4e788bfb123
Muhammad Hassan <hassan.nust@gmail.com>
2020-05-10 22:17:21 +0200

    no message

--- dqn/exercise/model.py
+++ dqn/exercise/model.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed):
+    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64):
         """Initialize parameters and build model.
         Params
         ======
@@ -16,7 +16,49 @@ class QNetwork(nn.Module):
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
         "*** YOUR CODE HERE ***"
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
 
     def forward(self, state):
         """Build a network that maps state -> action values."""
-        pass
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+
+# without convolutional networks DuelingDQN
+class DuelingDQN(nn.Module):
+
+    def __init__(self, input_dim, output_dim, seed):
+        super(DuelingDQN, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+        self.feauture_layer = nn.Sequential(
+            nn.Linear(self.input_dim[0], 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU()
+        )
+
+        self.value_stream = nn.Sequential(
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
+
+        self.advantage_stream = nn.Sequential(
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, self.output_dim)
+        )
+
+    def forward(self, state):
+        features = self.feauture_layer(state)
+        values = self.value_stream(features)
+        advantages = self.advantage_stream(features)
+        qvals = values + (advantages - advantages.mean())
+
+        return qvals
